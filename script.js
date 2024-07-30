9f781500fd7bd4de125bc89ff6e980ecd1aeb48f
wakiryounus <wakiryounus@gmail.com>
2024-07-30 06:53:01 +0530

    printNos

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const printNos = (n, result) => {
+  if (n > 0) {
+      result = result ? result : []
+      result.push(n)
+      return printNos(n - 1, result)
+  }
+  return result.reverse()
+}
